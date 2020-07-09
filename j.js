0715f23355f7e254df195fe8876909f907c5456b
Apple <apple@Apples-MacBook-Pro.local>
2020-07-09 19:23:14 +0700

    java

---
+++ j.js
@@ -0,0 +1,5 @@
+let inputwidth = prompt("enter the width");parseInt(inputwidth);
+let inputheight = prompt("enter the height");parseInt(inputheight);
+let area = inputwidth * inputheight;
+document.write("the area is:" + area)
+
